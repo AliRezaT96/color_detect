ca97fe1a7925177cadbc09859c926900827e848c
AliRezaT96 <alireza.toghroli1996@gmail.com>
2022-12-04 09:05:29 +0330

    bbox added in input

--- inference.py
+++ inference.py
@@ -8,14 +8,17 @@ import numpy as np
 #Creating argument parser to take image path from command line
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help="Image Path")
+ap.add_argument('-b', '--bbox', required=True, help="bounding box")
 args = vars(ap.parse_args())
 img_path = args['image']
+bbox = args['bbox'].split(',')
+b_box = [int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])]
 
 #Reading csv file with pandas and giving names to each column
 index=["color","color_name","hex","R","G","B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
 
-out = color_detection(img_path,[130,160,100,400])
+out = color_detection(img_path,b_box)
 
 r , g , b = 0 , 0 , 0
 #Creating text string to display( Color name and RGB values )
